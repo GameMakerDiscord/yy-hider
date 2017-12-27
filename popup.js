cd30c53c364f4141748648c7293751b3067902f8
Chris Anselmo <canselmo@hybridhealth.com>
2017-12-27 07:53:24 -0800

    Add comments

--- popup.js
+++ popup.js
@@ -1,20 +1,32 @@
+// Grab the setting box element
 var settingBox = document.getElementById('setting');
 
+// Load previous setting
 chrome.storage.sync.get({
-  noYYdelay: 2000
+  noYYdelay: 1000
 }, function(items) {
   settingBox.value = items.noYYdelay;
+
+  // Re-set the value, in case it hasn't been set before
+  chrome.storage.sync.set({
+    noYYdelay: settingBox.value
+  });
 });
 
+// Listen for inputs
 settingBox.addEventListener('input', function(e) {
+
+  // No decimals!
   settingBox.value = Math.round(settingBox.value);
 
+  // Keep in bounds
   if (settingBox.value < 0) {
     settingBox.value = 0;
   } else if (settingBox.value > 10000) {
     settingBox.value = 10000;
   }
 
+  // Save settings
   chrome.storage.sync.set({
     noYYdelay: settingBox.value
   });
--- yyhider.js
+++ yyhider.js
@@ -1,20 +1,32 @@
-var delay = 2000;
+// Init delay
+var delay = 1000;
 
+// Load saved delay value
 chrome.storage.sync.get({
-  noYYdelay: 2000
+  noYYdelay: 1000
 }, function(items) {
   delay = parseInt(items.noYYdelay);
 });
 
+// Sweep on interval
 setInterval(function() {
-  console.log('test')
+
+  // Query on file boxes
   var fileBoxes = document.querySelectorAll('.file');
 
+  // Iterate boxes
   fileBoxes.forEach(function(fileBox) {
+
+    // Search for info links (file title)
     var fileTitle = fileBox.querySelector('.file-info').querySelector('a');
     
+    // Search for presence of .yy extension
     if (~fileTitle.text.toLowerCase().indexOf('.yy')) {
+      
+      // DELETE
       fileBox.style.display = 'none';
     }
   });
+
+// Use loaded delay
 }, delay);
